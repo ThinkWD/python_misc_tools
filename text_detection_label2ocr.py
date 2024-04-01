ad2fa9b730ac413135cce2b6a5eb1e032dd2213e
ThinkWD <thinkwd@foxmail.com>
2024-04-01 20:26:01 +0800

    整理代码, 添加缩放功能

--- text_detection_label2ocr.py
+++ text_detection_label2ocr.py
@@ -9,9 +9,9 @@ from tqdm import tqdm
 #
 #   此文件用于文本检测数据集转换格式
 #
-#   1.从 labelimg 矩形标注裁出小图, 保存到文件
-#   2.从 labelme 多边形标注提取文本框在小图上的相对坐标
-#   3.转为 mmocr 或 paddocr 训练所需的格式.
+#   1. 从 labelimg 矩形标注裁出小图, 缩放后(可选), 保存到文件
+#   2. 从 labelme 多边形标注提取文本框在小图上的相对坐标
+#   3. 转为 mmocr 或 paddocr 训练所需的格式.
 #
 ##################################################################
 
@@ -103,77 +103,117 @@ def parse_labelme(json_path, image_width, image_height):
     return shape_labels, shape_points, shape_center
 
 
+def generate_labelme_check_file(shapes, width, height, save_path, save_name):
+    check_json = dict(
+        version='5.1.1',
+        flags={},
+        shapes=[],
+        imagePath=f'./{save_name}.jpg',
+        imageData=None,
+        imageHeight=height,
+        imageWidth=width,
+    )
+    for shape in shapes:
+        element = dict(
+            label='Null',
+            points=shape.tolist(),
+            group_id=None,
+            shape_type='polygon',
+            flags={},
+        )
+        check_json['shapes'].append(element)
+    with open(f"{save_path}/{save_name}.json", "w") as f:
+        json.dump(check_json, f, indent=4)
+
+
+def generate_format_label_string(labels, shapes, width, height, relative_path, format="paddle"):
+    if format == "paddle":
+        anns = []
+        for i, shape in enumerate(shapes):
+            anns.append({"transcription": labels[i], "points": shape.tolist()})
+        result = f"{relative_path}\t{json.dumps(anns, ensure_ascii=False)}\n"
+
+    elif format == "mmlab":
+        anns = []
+        for i, shape in enumerate(shapes):
+            ns_tl = shape.min(axis=0).tolist()
+            ns_br = shape.max(axis=0).tolist()
+            ann = {
+                "iscrowd": 0,
+                "category_id": 1,
+                "bbox": [ns_tl[0], ns_tl[1], ns_br[0] - ns_tl[0], ns_br[1] - ns_tl[1]],
+                "segmentation": shape.reshape(1, -1).tolist(),
+                "text": labels[i],
+            }
+            anns.append(ann)
+        result = {"file_name": relative_path, "height": height, "width": width, "annotations": anns}
+        result = f"{json.dumps(result, ensure_ascii=False)}\n"
+
+    else:
+        print("Only support Paddle OCR format and mmlab OCR format")
+        exit()
+
+    return result
+
+
 # 单个图片
-def generate(img_path, xml_path, json_path, save_dir, save_file, keep_ratio, format="paddle"):
+def generate(img_path, xml_path, json_path, keep_ratio, save_root, save_relative, save_name, resize=736):
     # check image
     assert os.path.isfile(img_path), f"图片文件不存在: {img_path}"
     img = PIL.Image.open(img_path)
-    width, height = img.size
-    assert width > 0 and height > 0
+    img_width, img_height = img.size
+    assert img_width > 0 and img_height > 0
     # parse labelme anns file
-    shape_labels, shape_points, shape_center = parse_labelme(json_path, width, height)
+    shape_labels, shape_points, shape_center = parse_labelme(json_path, img_width, img_height)
     # parse labelimg anns file
+    bbox_dict = parse_labelimg(xml_path, img_width, img_height)
+    # Start loop
     label_string = []
-    bbox_dict = parse_labelimg(xml_path, width, height)
+    save_path = os.path.join(save_root, save_relative)
     for idx, box in enumerate(bbox_dict):
-        anns = []
-        if keep_ratio:
-            box_width = box[2] - box[0]
-            box_height = box[3] - box[1]
-            img_length = max(box_width, box_height)
-            offset = np.array([max((img_length - box_width) // 2, 0), max((img_length - box_height) // 2, 0)])
-        # generate paddleocr label string
+        # get shapes
         box = np.array(box)  # 将矩形框转换为 numpy 数组
+        shapes = []
         for i, shape in enumerate(shape_points):
             if not rectangle_include_point(box, shape_center[i]):
                 continue
-            # 将多边形约束到矩形范围内
-            new_shape = shape
+            new_shape = shape  # 将多边形约束到矩形范围内
             for p in new_shape:
                 p[0] = max(box[0], min(p[0], box[2]))
                 p[1] = max(box[1], min(p[1], box[3]))
-            new_shape = new_shape - box[:2]
-            if keep_ratio:
-                new_shape = new_shape + offset
+            shapes.append(new_shape - box[:2])
+        if len(shapes) == 0:
+            continue
 
-            if format == "paddle":
-                ann = {"transcription": shape_labels[i], "points": new_shape.tolist()}
-            elif format == "mmlab":
-                ns_tl = new_shape.min(axis=0).tolist()
-                ns_br = new_shape.max(axis=0).tolist()
-                ann = {
-                    "iscrowd": 0,
-                    "category_id": 1,
-                    "bbox": [ns_tl[0], ns_tl[1], ns_br[0] - ns_tl[0], ns_br[1] - ns_tl[1]],
-                    "segmentation": new_shape.reshape(1, -1).tolist(),
-                    "text": shape_labels[i],
-                }
-            else:
-                print("Only support Paddle OCR format and mmlab OCR format")
-                exit()
+        # organize path
+        raw_name = f"{save_name}_{idx}"
+        rel_path = f"{save_relative}/{raw_name}.jpg"  # relative_path
 
-            anns.append(ann)
         # crop and save crop img
-        if len(anns) > 0:
-            path = f"{save_file}_{idx}.jpg"
-            crop_img = img.crop(box).convert("RGB")
-            if keep_ratio:
-                res = PIL.Image.new("RGB", (img_length, img_length), (0, 0, 0))
-                res.paste(crop_img, (offset[0], offset[1]))
-                res.save(os.path.join(save_dir, path))
-            else:
-                crop_img.save(os.path.join(save_dir, path))
+        box_width = int(box[2] - box[0])
+        box_height = int(box[3] - box[1])
+        crop_img = img.crop(box).convert("RGB")
+        if keep_ratio:
+            # update params
+            img_length = max(box_width, box_height)
+            offset = np.array([max((img_length - box_width) // 2, 0), max((img_length - box_height) // 2, 0)])
+            scale = resize / img_length if resize > img_length else 1
+            box_width = resize if resize > img_length else img_length
+            box_height = resize if resize > img_length else img_length
+            for i in range(len(shapes)):
+                shapes[i] = (shapes[i] + offset) * scale
+            # pad and resize image
+            res = PIL.Image.new("RGB", (img_length, img_length), (0, 0, 0))
+            res.paste(crop_img, (offset[0], offset[1]))
+            crop_img = res.resize((box_width, box_height), PIL.Image.BILINEAR)
+        crop_img.save(f"{save_path}/{raw_name}.jpg")
 
-            if format == "paddle":
-                result = f"{path}\t{json.dumps(anns, ensure_ascii=False)}\n"
-            elif format == "mmlab":
-                result = {"file_name": path, "height": height, "width": width, "annotations": anns}
-                result = f"{json.dumps(result, ensure_ascii=False)}\n"
-            else:
-                print("Only support Paddle OCR format and mmlab OCR format")
-                exit()
+        # generate anns label string
+        label_string.append(generate_format_label_string(shape_labels, shapes, box_width, box_height, rel_path))
 
-            label_string.append(result)
+        # generate labelme check file
+        generate_labelme_check_file(shapes, box_width, box_height, save_path, raw_name)
+
     return label_string
 
 
@@ -181,7 +221,8 @@ def process(root_path, save_dir, split, keep_ratio):
     work_path = os.path.join(root_path, "src")
     save_path = os.path.join(root_path, save_dir)
     assert os.path.isdir(work_path), f"数据集不存在: {work_path}"
-    os.makedirs(save_path)
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
 
     print(f"\n[info] start task...")
     with open(f"{save_path}/all_list.txt", "w", encoding='utf-8') as ann_file:
@@ -198,15 +239,15 @@ def process(root_path, save_dir, split, keep_ratio):
             assert os.path.isdir(img_path), f"图片文件夹不存在: {img_path}"
             img_list = [f for f in os.listdir(img_path) if f.lower().endswith((".jpg", ".jpeg", ".png"))]
             # 遍历 ann 文件列表
+            save_relative = f"dataset/{pre_dir}"
             for file in tqdm(img_list, desc=f"{pre_dir}\t", leave=True, ncols=100, colour="CYAN"):
                 # 获取文件名(带多文件夹的相对路径)
                 raw_name, extension = os.path.splitext(file)
-                img_path = f"src/{pre_dir}/imgs/{raw_name}{extension}"
-                xml_path = f"src/{pre_dir}/anns/{raw_name}.xml"
-                json_path = f"src/{pre_dir}/anns_seg/{raw_name}.json"
-                img_save_path = f"dataset/{pre_dir}/{raw_name}"
+                img_path = f"{work_path}/{pre_dir}/imgs/{raw_name}{extension}"
+                xml_path = f"{work_path}/{pre_dir}/anns/{raw_name}.xml"
+                json_path = f"{work_path}/{pre_dir}/anns_seg/{raw_name}.json"
                 # 解析单个 ann 文件
-                label_string = generate(img_path, xml_path, json_path, save_dir, img_save_path, keep_ratio)
+                label_string = generate(img_path, xml_path, json_path, keep_ratio, save_path, save_relative, raw_name)
                 for str in label_string:
                     ann_file.write(str)
     with open(f"{save_path}/all_list.txt", "r", encoding='utf-8') as f:
