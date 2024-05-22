ce57aa4740ba093f5481bf54a39529f75da9bbfc
ThinkWD <thinkwd@foxmail.com>
2024-05-22 13:37:46 +0800

    为 segmentation_labelme2seg.py 添加皮带跑偏数据集所需的旋转功能支持

--- segmentation_labelme2seg.py
+++ segmentation_labelme2seg.py
@@ -82,6 +82,16 @@ def shape2label(img_size, shapes, class_name_mapping):
     return label
 
 
+def data_shapes_rotate(shapes, width, height):
+    x_scale = height / width
+    y_scale = width / height
+    for shape in shapes:
+        for pt in shape['points']:
+            x_tmp = width - pt[0]
+            pt[0] = pt[1] * y_scale
+            pt[1] = x_tmp * x_scale
+
+
 def main(root_path, split_ratio, format="paddle"):
     # init class_names
     class_names = ['__ignore__', '_background_', 'belt_L', 'belt_R', 'roller_L', 'roller_R']  # 0: 刻度, 1: 指针
@@ -89,6 +99,7 @@ def main(root_path, split_ratio, format="paddle"):
     assert class_name_to_id['__ignore__'] == -1
     assert class_name_to_id['_background_'] == 0
     class_names = tuple(class_names)
+    color_map = get_color_map_list(256)
 
     # get path
     imgs_path = os.path.join(root_path, "imgs")
@@ -98,53 +109,74 @@ def main(root_path, split_ratio, format="paddle"):
     assert not os.path.isdir(png_path), "anns_png directory already exists"
 
     # start work
-    color_map = get_color_map_list(256)
-    with open(os.path.join(root_path, "all_list.txt"), "a") as f:
-        for dir in find_dir(imgs_path):
-            pre_dir = os.path.basename(dir)  # 获取并打印子文件夹名
-            os.makedirs(os.path.join(png_path, pre_dir))
-            imgs_list = [f for f in os.listdir(dir) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
-            # 遍历图片列表
-            for file in tqdm(imgs_list, desc=f"{pre_dir}\t", leave=True, ncols=100, colour="CYAN"):
-                raw_name, extension = os.path.splitext(file)
-                imgpath = f"{imgs_path}/{pre_dir}/{raw_name}{extension}"
-                # check ann file
-                annpath = f"{anns_path}/{pre_dir}/{raw_name}.json"
-                assert os.path.isfile(annpath)
-                # parse ann file
-                with open(annpath, "r+", encoding="utf-8") as file:
-                    data = json.load(file)
-                    data["imageData"] = None
-                    data["imagePath"] = f"../../imgs/{pre_dir}/{raw_name}{extension}"
-                    # 保存修改后的文件
-                    file.seek(0)
-                    file.truncate()
-                    file.write(json.dumps(data))
-                # 获取图片长宽
-                width, height = PIL.Image.open(imgpath).size
-                assert width > 0 and height > 0
-                # 生成 mask
-                mask = shape2label([height, width], data['shapes'], class_name_to_id)
-                # Assume label ranges [0, 255] for uint8,
-                if mask.min() < 0 or mask.max() > 255:
-                    raise Exception(f'[{annpath}] Cannot save the pixel-wise class label as PNG.')
-                lbl_pil = PIL.Image.fromarray(mask.astype(np.uint8), mode='P')
-                lbl_pil.putpalette(color_map)
-                lbl_pil.save(f"{png_path}/{pre_dir}/{raw_name}.png")
-                if format == "paddle":
-                    f.write(f"imgs/{pre_dir}/{raw_name}{extension} anns_png/{pre_dir}/{raw_name}.png\n")
-                elif format == "mmlab":
-                    f.write(f"{pre_dir}/{raw_name}\n")
-                else:
-                    raise Exception("Only support Paddle OCR format and mmlab OCR format")
-    with open(os.path.join(root_path, "all_list.txt"), "r") as f:
-        list_train = f.readlines()
-    list_test = list_train[::split_ratio]
-    with open(os.path.join(root_path, "test.txt"), "a") as file:
-        file.writelines(list_test)
-    del list_train[::split_ratio]
-    with open(os.path.join(root_path, "train.txt"), "a") as file:
-        file.writelines(list_train)
+    dataset = []
+    for dir in find_dir(imgs_path):
+        prefix = os.path.basename(dir)
+        if prefix.endswith('_rotated'):
+            print(f"Easy to confuse directories: {prefix}, continue.")
+            continue
+        rotate_mode = True if prefix.endswith('_R') else False
+        if not rotate_mode:
+            os.makedirs(os.path.join(png_path, prefix))
+        else:
+            new_prefix = f"{prefix[:-1]}rotated"
+            os.makedirs(os.path.join(imgs_path, new_prefix), exist_ok=True)
+            os.makedirs(os.path.join(anns_path, new_prefix), exist_ok=True)
+            os.makedirs(os.path.join(png_path, new_prefix))
+        used_prefix = new_prefix if rotate_mode else prefix
+        imgs_list = [f for f in os.listdir(dir) if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+        # 遍历图片列表
+        for file in tqdm(imgs_list, desc=f"{prefix}\t", leave=True, ncols=100, colour="CYAN"):
+            raw_name, extension = os.path.splitext(file)
+            imgpath = f"{imgs_path}/{prefix}/{raw_name}{extension}"
+            # check image size
+            cur_image = PIL.Image.open(imgpath)
+            width, height = cur_image.size
+            assert width > 0 and height > 0
+            if rotate_mode:
+                cur_image = cur_image.rotate(90, expand=True)
+                cur_image = cur_image.resize((width, height))
+                cur_image.save(f'{imgs_path}/{new_prefix}/{raw_name}{extension}')
+            # check ann file
+            annpath = f"{anns_path}/{prefix}/{raw_name}.json"
+            assert os.path.isfile(annpath)
+            # parse ann file
+            with open(annpath, "r+", encoding="utf-8") as file:
+                data = json.load(file)
+                assert width == data["imageWidth"] and height == data["imageHeight"]
+                data["imageData"] = None
+                data["imagePath"] = f"../../imgs/{prefix}/{raw_name}{extension}"
+                file.seek(0)
+                file.truncate()
+                file.write(json.dumps(data, indent=4))
+            if rotate_mode:
+                data_shapes_rotate(data['shapes'], width, height)
+                data["imagePath"] = f"../../imgs/{new_prefix}/{raw_name}{extension}"
+                with open(f'{anns_path}/{new_prefix}/{raw_name}.json', "w") as new_file:
+                    new_file.write(json.dumps(data, indent=4))
+            # generate mask
+            mask = shape2label([height, width], data['shapes'], class_name_to_id)
+            # Assume label ranges [0, 255] for uint8,
+            if mask.min() < 0 or mask.max() > 255:
+                raise Exception(f'[{annpath}] Cannot save the pixel-wise class label as PNG.')
+            lbl_pil = PIL.Image.fromarray(mask.astype(np.uint8), mode='P')
+            lbl_pil.putpalette(color_map)
+            lbl_pil.save(f"{png_path}/{used_prefix}/{raw_name}.png")
+            if format == "paddle":
+                dataset.append(f"imgs/{used_prefix}/{raw_name}{extension} anns_png/{used_prefix}/{raw_name}.png\n")
+            elif format == "mmlab":
+                dataset.append(f"{used_prefix}/{raw_name}\n")
+            else:
+                raise Exception("Only support Paddle OCR format and mmlab OCR format")
+
+    with open(os.path.join(root_path, "all_list.txt"), "w") as file:
+        file.writelines(dataset)
+    test_data = dataset[::split_ratio]
+    with open(os.path.join(root_path, "test_list.txt"), "w") as file:
+        file.writelines(test_data)
+    del dataset[::split_ratio]
+    with open(os.path.join(root_path, "train_list.txt"), "w") as file:
+        file.writelines(dataset)
 
 
 # Reference: https://github.com/wkentaro/labelme/blob/main/examples/semantic_segmentation/labelme2voc.py
